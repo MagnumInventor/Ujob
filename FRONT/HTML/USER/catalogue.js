e11e164a851fcba5d232f5d82dfe4bf0e3adc8a8
Олександр Маркович <markoleks08@gmail.com>
2024-12-21 19:50:35 +0200

    submit Fun BLOCKED

--- FRONT/HTML/USER/catalogue.js
+++ FRONT/HTML/USER/catalogue.js
@@ -1,11 +1,11 @@
-// Дочекатися завантаження DOM
 document.addEventListener('DOMContentLoaded', () => {
     const vacancyButton = document.getElementById('open-vacancy-button');
     const cardBody = document.getElementById('card-body');
 
-    if (vacancyButton && cardBody) { // Перевірка, чи існують елементи
-        vacancyButton.addEventListener('click', () => {
-            cardBody.classList.toggle('active'); // Переключення класу 'active'
+    if (vacancyButton && cardBody) {
+        vacancyButton.addEventListener('click', (event) => {
+            event.preventDefault(); // Запобігаємо перезавантаженню сторінки
+            cardBody.classList.toggle('active');
             console.log('Тиснули на кнопку, клас активується:', cardBody.classList.contains('active'));
         });
     } else {
