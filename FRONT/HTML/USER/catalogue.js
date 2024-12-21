8fb158132203b609167cad46ac2a306e19410398
Олександр Маркович <markoleks08@gmail.com>
2024-12-21 19:47:17 +0200

    ++JS

--- FRONT/HTML/USER/catalogue.js
+++ FRONT/HTML/USER/catalogue.js
@@ -3,8 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const vacancyButton = document.getElementById('open-vacancy-button');
     const cardBody = document.getElementById('card-body');
 
-    // Додавання обробника подій для кнопки
-    vacancyButton.addEventListener('click', () => {
-        cardBody.classList.toggle('active');
-    });
+    if (vacancyButton && cardBody) { // Перевірка, чи існують елементи
+        vacancyButton.addEventListener('click', () => {
+            cardBody.classList.toggle('active'); // Переключення класу 'active'
+            console.log('Тиснули на кнопку, клас активується:', cardBody.classList.contains('active'));
+        });
+    } else {
+        console.error('Елементи button або card-body не знайдено!');
+    }
 });
